c188803a564488eaa7ba40d82d12a04d0336dedb
juanqenk <juanqenk@gmail.com>
2025-03-04 12:34:55 +0100

    change menu

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1740868198',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1741088026',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/apuntes/', '/cuaderno-de-campo/', '/recetas/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
